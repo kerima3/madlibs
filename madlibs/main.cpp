d4e5666798002ec524bf542ea29980604e67c545
Kerima Hussen <kerima.mh3@gmail.com>
2021-11-25 13:48:27 -0800

    Initial Commit

---
+++ madlibs/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  madlibs
+//
+//  Created by Kerima Hussen on 11/24/21.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    std:: string noun, adjective, noun2;
+    
+    std:: cout << "Enter a noun: " << noun << "\n";
+    std:: cin >> noun;
+    
+    std:: cout << "Enter an adjective: " << adjective << "\n";
+    std:: cin >> adjective;
+    
+    std:: cout << "Enter a second noun: " << noun2 << "\n";
+    std:: cin >> noun2;
+    
+    std:: cout << "After hiding the painting in his " << noun << " for two" << "\n";
+    std:: cout << "years, he grew " << adjective << " and tried to sell it" << "\n";
+    std:: cout << "to a/an " << noun2 << " in Florence, but was cought." << "\n";
+    
+    
+}
+
